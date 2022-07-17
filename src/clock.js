95a6616570acd97562c64d442ce7c6979bfcadc2
lapuger <53108649+lapuger@users.noreply.github.com>
2022-07-18 00:17:55 +0900

    Update clock.js

--- src/clock.js
+++ src/clock.js
@@ -12,5 +12,18 @@ function getClock() {
     setInterval(getClock, 1000);
 
 
+const time = document.querySelector("#wrapper .clock");
 
+function handlerMouseEnter() {
+    time.style.opacity = 1;
+    time.style.transition = "0.2s"
+    time.querySelector(".clock-setting").classList.remove("hidden");
+};
+function handlerMouseLeave() {
+    time.style.opacity = 0.6;
+    time.style.transition = "0.2s";
+    time.querySelector(".clock-setting").classList.add("hidden");
+};
 
+time.addEventListener("mouseenter", handlerMouseEnter);
+time.addEventListener("mouseleave", handlerMouseLeave);
