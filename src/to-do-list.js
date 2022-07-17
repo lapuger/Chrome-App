9f193d5c9f7c8a6e5439f3f55fa68a99d064aee5
lapuger <53108649+lapuger@users.noreply.github.com>
2022-07-18 00:16:38 +0900

    Update to-do-list.js

--- src/to-do-list.js
+++ src/to-do-list.js
@@ -1,22 +1,3 @@
-/*
-const toDoShortCut = document.querySelector("#wrapper .to-do-shortcut");
-
-function handlerMouseEnter() {
-    toDoShortCut.style.opacity = 1;
-    toDoShortCut.style.transition = "0.2s"
-};
-function handlerMouseLeave() {
-    toDoShortCut.style.opacity = 0.6;
-    toDoShortCut.style.transition = "0.2s";
-};
-
-
-toDoShortCut.addEventListener("mouseenter", handlerMouseEnter);
-toDoShortCut.addEventListener("mouseleave", handlerMouseLeave);
-
-//todo shortcut mouse event
-*/
-
 
 const toDoForm = document.getElementById("to-do-form");
 const toDoInput = toDoForm.querySelector("input");
